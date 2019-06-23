d24f1370423709b2d6df241f6f73771dd1d69695
masrobot <muhamadmashudiardiwinata@gmail.com>
2019-06-23 19:47:09 +0700

    finish movie data

---
+++ app/src/main/java/c/dicodingmade/fragment/movie/MovieAdapter.kt
@@ -0,0 +1,45 @@
+package c.dicodingmade.fragment.movie
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import c.dicodingmade.R
+import c.dicodingmade.model.MovieData
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_list_movie_tv_show.*
+
+class MovieAdapter(
+    private val movieData: List<MovieData>,
+    private val listener: (MovieData) -> Unit
+) :
+    RecyclerView.Adapter<MovieViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder = MovieViewHolder(
+        LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list_movie_tv_show, parent, false)
+    )
+
+    override fun getItemCount(): Int = movieData.size
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) =
+        holder.bindItem(movieData[position], listener)
+}
+
+class MovieViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    fun bindItem(movieData: MovieData, listener: (MovieData) -> Unit) {
+        layout_item_movie_tv_show.setOnClickListener { listener(movieData) }
+        Glide.with(img_poster.context)
+            .load(movieData.moviePoster)
+            .apply(
+                RequestOptions()
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_error)
+            )
+            .into(img_poster)
+        tv_title.text = movieData.movieTitle
+        tv_release_date.text = movieData.movieReleaseDate
+        tv_description.text = movieData.movieDescription
+    }
+}
--- app/src/main/java/c/dicodingmade/fragment/movie/MovieFragment.kt
+++ app/src/main/java/c/dicodingmade/fragment/movie/MovieFragment.kt
@@ -1,14 +1,24 @@
 package c.dicodingmade.fragment.movie
 
 
+import android.content.res.TypedArray
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import c.dicodingmade.R
+import c.dicodingmade.model.MovieData
+import kotlinx.android.synthetic.main.fragment_movie.*
 
 class MovieFragment : Fragment() {
+    private var listMovies: MutableList<MovieData> = mutableListOf()
+    private lateinit var movieAdapter: MovieAdapter
+    private lateinit var moviePosterData: TypedArray
+    private var movieTitleData: Array<String> = arrayOf()
+    private var movieReleaseDateData: Array<String> = arrayOf()
+    private var movieDescriptionData: Array<String> = arrayOf()
+    private var movies: ArrayList<MovieData> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +28,42 @@ class MovieFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_movie, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        movieRecyclerView()
+        dataPreparation()
+        addDataMovie()
+    }
+
+    private fun movieRecyclerView() {
+        movieAdapter = MovieAdapter(listMovies) {
+
+        }
+        rv_movie.adapter = movieAdapter
+        //rv_movie.layoutManager = LinearLayoutManager(context)
+        rv_movie.isNestedScrollingEnabled = false
+    }
+
+    private fun addDataMovie() {
+        for (i in 0 until movieTitleData.size) {
+            val movieData = MovieData()
+            movieData.moviePoster = moviePosterData.getResourceId(i, -i)
+            movieData.movieTitle = movieTitleData[i]
+            movieData.movieReleaseDate = movieReleaseDateData[i]
+            movieData.movieDescription = movieDescriptionData[i]
+            movies.add(movieData)
+        }
+
+        listMovies.clear()
+        listMovies.addAll(movies)
+        movieAdapter.notifyDataSetChanged()
+    }
+
+    private fun dataPreparation() {
+        moviePosterData = resources.obtainTypedArray(R.array.poster_movie_data)
+        movieTitleData = resources.getStringArray(R.array.title_movie_data)
+        movieReleaseDateData = resources.getStringArray(R.array.movie_release_date_data)
+        movieDescriptionData = resources.getStringArray(R.array.movie_description_data)
+    }
 }
