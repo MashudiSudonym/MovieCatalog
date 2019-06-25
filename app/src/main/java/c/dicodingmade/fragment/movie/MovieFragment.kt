3f96eca666de36852db28c2ef1b411d89c1f2b0f
masrobot <muhamadmashudiardiwinata@gmail.com>
2019-06-25 23:17:01 +0700

    setup submission 3

--- app/src/main/java/c/dicodingmade/fragment/movie/MovieFragment.kt
+++ app/src/main/java/c/dicodingmade/fragment/movie/MovieFragment.kt
@@ -1,26 +1,14 @@
 package c.dicodingmade.fragment.movie
 
 
-import android.content.Intent
-import android.content.res.TypedArray
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import c.dicodingmade.R
-import c.dicodingmade.activity.detail.DetailActivity
-import c.dicodingmade.model.MovieTvShowData
-import kotlinx.android.synthetic.main.fragment_movie.*
 
 class MovieFragment : Fragment() {
-    private var listMovieTvShows: MutableList<MovieTvShowData> = mutableListOf()
-    private lateinit var movieAdapter: MovieAdapter
-    private lateinit var moviePosterData: TypedArray
-    private var movieTitleData: Array<String> = arrayOf()
-    private var movieReleaseDateData: Array<String> = arrayOf()
-    private var movieDescriptionData: Array<String> = arrayOf()
-    private var movies: ArrayList<MovieTvShowData> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,47 +21,5 @@ class MovieFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        movieRecyclerView()
-        dataPreparation()
-        addDataMovie()
-    }
-
-    private fun movieRecyclerView() {
-        movieAdapter = MovieAdapter(listMovieTvShows) {
-            val singleMovieData = MovieTvShowData().apply {
-                movieTvShowPoster = it.movieTvShowPoster
-                movieTvShowTitle = it.movieTvShowTitle
-                movieTvShowReleaseDate = it.movieTvShowReleaseDate
-                movieTvShowDescription = it.movieTvShowDescription
-            }
-            Intent(context, DetailActivity::class.java)
-                .putExtra(DetailActivity.EXTRA_DATA, singleMovieData).also {
-                    startActivity(it)
-                }
-        }
-        rv_movie.adapter = movieAdapter
-        rv_movie.isNestedScrollingEnabled = false
-    }
-
-    private fun addDataMovie() {
-        for (i in movieTitleData.indices) {
-            val movieData = MovieTvShowData()
-            movieData.movieTvShowPoster = moviePosterData.getResourceId(i, -i)
-            movieData.movieTvShowTitle = movieTitleData[i]
-            movieData.movieTvShowReleaseDate = movieReleaseDateData[i]
-            movieData.movieTvShowDescription = movieDescriptionData[i]
-            movies.add(movieData)
-        }
-
-        listMovieTvShows.clear()
-        listMovieTvShows.addAll(movies)
-        movieAdapter.notifyDataSetChanged()
-    }
-
-    private fun dataPreparation() {
-        moviePosterData = resources.obtainTypedArray(R.array.poster_movie_data)
-        movieTitleData = resources.getStringArray(R.array.title_movie_data)
-        movieReleaseDateData = resources.getStringArray(R.array.movie_release_date_data)
-        movieDescriptionData = resources.getStringArray(R.array.movie_description_data)
     }
 }
--- app/src/main/java/c/dicodingmade/fragment/tvshow/TvShowFragment.kt
+++ app/src/main/java/c/dicodingmade/fragment/tvshow/TvShowFragment.kt
@@ -1,26 +1,14 @@
 package c.dicodingmade.fragment.tvshow
 
 
-import android.content.Intent
-import android.content.res.TypedArray
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import c.dicodingmade.R
-import c.dicodingmade.activity.detail.DetailActivity
-import c.dicodingmade.model.MovieTvShowData
-import kotlinx.android.synthetic.main.fragment_tv_show.*
 
 class TvShowFragment : Fragment() {
-    private var listTvShow: MutableList<MovieTvShowData> = mutableListOf()
-    private lateinit var tvShowAdapter: TvShowAdapter
-    private lateinit var tvShowPosterData: TypedArray
-    private var tvShowTitleData: Array<String> = arrayOf()
-    private var tvShowReleaseDateData: Array<String> = arrayOf()
-    private var tvShowDescriptionData: Array<String> = arrayOf()
-    private var tvShows: ArrayList<MovieTvShowData> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,43 +21,5 @@ class TvShowFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tvShowAdapter = TvShowAdapter(listTvShow) {
-            val singleTvShowData = MovieTvShowData().apply {
-                movieTvShowPoster = it.movieTvShowPoster
-                movieTvShowTitle = it.movieTvShowTitle
-                movieTvShowReleaseDate = it.movieTvShowReleaseDate
-                movieTvShowDescription = it.movieTvShowDescription
-            }
-            Intent(context, DetailActivity::class.java)
-                .putExtra(DetailActivity.EXTRA_DATA, singleTvShowData).also {
-                    startActivity(it)
-                }
-        }
-        rv_tv_show.adapter = tvShowAdapter
-        rv_tv_show.isNestedScrollingEnabled = false
-        dataPreparation()
-        addDataTvShow()
-    }
-
-    private fun addDataTvShow() {
-        for (i in tvShowTitleData.indices) {
-            val tvShowData = MovieTvShowData()
-            tvShowData.movieTvShowPoster = tvShowPosterData.getResourceId(i, -1)
-            tvShowData.movieTvShowTitle = tvShowTitleData[i]
-            tvShowData.movieTvShowReleaseDate = tvShowReleaseDateData[i]
-            tvShowData.movieTvShowDescription = tvShowDescriptionData[i]
-            tvShows.add(tvShowData)
-        }
-
-        listTvShow.clear()
-        listTvShow.addAll(tvShows)
-        tvShowAdapter.notifyDataSetChanged()
-    }
-
-    private fun dataPreparation() {
-        tvShowPosterData = resources.obtainTypedArray(R.array.poster_tv_show_data)
-        tvShowTitleData = resources.getStringArray(R.array.title_tv_show_data)
-        tvShowReleaseDateData = resources.getStringArray(R.array.tv_show_release_date_data)
-        tvShowDescriptionData = resources.getStringArray(R.array.tv_show_description_data)
     }
 }
